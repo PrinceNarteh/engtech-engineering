a7041905d5e3fbb6b67f92994aa7b288987db44f
Prince Narteh <prinarteh@gmail.com>
2024-07-22 04:23:46 +0000

    feat: add shadcn/ui framework

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const font = localFont({
+const fontSans = localFont({
   src: [
     {
       path: "../assets/fonts/Archivo-Regular.woff2",
@@ -40,7 +41,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${font.className} font-sans`}>{children}</body>
+      <body className={cn("font-archivo", fontSans.variable)}>{children}</body>
     </html>
   );
 }
