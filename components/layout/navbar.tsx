d06e9a647c9c361c5e160aaebd28d60d0113ec29
Prince Narteh <prinarteh@gmail.com>
2024-07-22 06:53:02 +0000

    feat: add navbar

---
+++ components/layout/navbar.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const links = [
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "Services",
+    path: "/services",
+  },
+  {
+    label: "Projects",
+    path: "/projects",
+  },
+  {
+    label: "Contact",
+    path: "/contact",
+  },
+];
+
+const Navbar = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="py-5 px-20">
+      <nav className="h-20 flex justify-between">
+        <div className="flex gap-2">
+          <Image
+            src="/images/logo.png"
+            alt="logo"
+            width={40}
+            height={40}
+            className="shrink-0 size-11"
+          />
+          <h3 className="font-bold leading-5 text-2xl">
+            <span className="text-orange-500">EngTech</span> <br />
+            <span>Engineering</span>
+          </h3>
+        </div>
+        <ul className="flex gap-5">
+          {links.map((link, idx) => {
+            const isActive = pathname === link.path;
+            return (
+              <li key={idx} className="text-lg font-semibold">
+                <Link
+                  href={link.path}
+                  className={`${isActive ? "text-orange-500" : ""}`}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
